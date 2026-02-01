f655cb2b6afe0f2b92b7f974674e6ff35b41f634
MoHassan <mahassn@mie.utoronto.ca>
2026-02-01 12:27:14 -0500

    Update wandb assertions in ci.py to check for loading instead of specific version

--- scripts/ci.py
+++ scripts/ci.py
@@ -1,5 +1,6 @@
 import wandb
 print(f'Ther version of wandb is {wandb.__version__}')
-
-assert wandb.__version__ == '0.24.0', f"WandB version is {wandb.__version__}, but 0.24.0 is required"
+# assert wandb is loaded
+assert wandb is not None, "WandB is not loaded"
+# assert wandb.__version__ == '0.24.0', f"WandB version is {wandb.__version__}, but 0.24.0 is required"
 
